9bb2f4434b980f47c51da8a57950ca0fcb7df7e3
s33440-cpu <s33440@pjwstk.edu.pl>
2026-03-24 01:14:25 +0100

    (#10)Test class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,41 @@
-﻿
+﻿using APBD_TASK2.Database;
+using APBD_TASK2.Enum;
+using APBD_TASK2.Models;
+using APBD_TASK2.Services;
 
-using APBD_TASK2.Database;
+var service = new RentalService();
 
-var db = Singleton.Instance;
+var student = new User("Ivan", "Dorokhov", "s33440@pjwstk.edu.pl", UserType.Student);
+var employee = new User("Navi", "Vohkorod", "04433s@pjwstk.edu.pl", UserType.Employee);
 
+service.AddUser(student);
+service.AddUser(employee);
+
+var laptop = new Laptop("Dell XPS", 16, 15);
+var projector = new Projector("Epson", 3000, true);
+var camera = new Camera("Canon", 24, "Zoom");
+
+service.AddEquipment(laptop);
+service.AddEquipment(projector);
+service.AddEquipment(camera);
+
+service.RentEquipment(student, laptop, 2);
+
+try
+{
+    service.RentEquipment(student, projector, 2);
+    service.RentEquipment(student, camera, 2);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+var rental = Singleton.Instance.Rentals.First();
+rental = Singleton.Instance.Rentals.First();
+rental.Return();
+
+double penalty = service.ReturnEquipment(rental);
+Console.WriteLine($"Penalty: {penalty}");
+
+Console.WriteLine(service.GenerateReport());
